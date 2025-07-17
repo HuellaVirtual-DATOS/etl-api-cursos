e31bf3c8192cc34aefd0060d4e1f97e122f29bb3
HuellaVirtual-DATOS <fbarrios.hvirtual@gmail.com>
2025-07-17 14:32:37 -0300

    Update ejecutar.sh

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Activar entorno virtual (opcional)
-# python3 -m venv venv
-# source venv/bin/activate
-
 # Instalar dependencias
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Ejecutar script principal
-python cargar_api_en_supabase.py
-
+python3 cargar_api_en_supabase.py
