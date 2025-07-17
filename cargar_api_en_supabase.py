b76a6ccd49053f9bc64161e080231ea365fe80b9
HuellaVirtual-DATOS <fbarrios.hvirtual@gmail.com>
2025-07-17 15:24:38 -0300

    Update cargar_api_en_supabase.py

--- cargar_api_en_supabase.py
+++ cargar_api_en_supabase.py
@@ -66,4 +66,9 @@ if __name__ == "__main__":
         print("Carga finalizada correctamente.")
     except Exception as e:
         print("Error durante el proceso:", e)
+        
+response = requests.get(url)
+print(response.text)  # <-- AÑADÍ ESTO TEMPORALMENTE
+data = response.json()
+
 
